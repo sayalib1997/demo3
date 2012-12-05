d243d0bd7646cc0acc61194a60a6ef4bf1afe69c
dumitval <dumitval@company.com>
2012-12-05 15:57:23 +0000

    #10139 FLIS Countries deployment
    Fix for supervisord.conf

--- flis_django/fabfile/__init__.py
+++ flis_django/fabfile/__init__.py
@@ -2,10 +2,14 @@ from fabric.api import *
 from fabric.contrib.files import *
 from path import path as ppath
 
+country_codes = ['pt', 'cz', 'be', 'ro', 'at', 'ir', 'sk', 'hu', 'me', 'mk',
+                 'al', 'xk']
+
 app = env.app = {
     'flis-repo': 'https://svn.eionet.europa.eu/repositories/Python/flis_django',
     'localrepo': ppath(__file__).abspath().parent.parent,
 }
+
 try: from localcfg import *
 except: pass
 
@@ -13,9 +17,15 @@ app.update({
     'instance_var': app['repo']/'instance',
     'flis_var': app['repo']/'flis',
     'sandbox': app['repo']/'sandbox',
+    'countries_sandbox': app['countries_repo']/'sandbox',
     'user': 'edw',
 })
 
+for country_code in country_codes:
+    app['%s-instance_var' % country_code] = '%s/%s/instance' % (
+                                        app['countries_repo'], country_code)
+    app['%s-flis_var' % country_code] = '%s/%s/flis' % (
+                                        app['countries_repo'], country_code)
 
 @task
 def ssh():
@@ -69,6 +79,44 @@ def install():
     run("%s/bin/python %s/manage.py migrate" % (app['sandbox'], app['repo']))
 
 @task
+def install_countries():
+    for country_code in country_codes:
+        _svn_repo(app['countries_repo']/country_code, app['flis-repo'], update=True)
+
+        if not exists(app['%s-instance_var' % country_code]):
+            run("mkdir -p %s" % app['%s-instance_var'] % country_code)
+        if not exists('%s/files' % app['%s-instance_var' % country_code]):
+            run("mkdir -p %s/files" % app['%s-instance_var' % country_code])
+
+        secret_key_path = '%s/secret_key.txt' % app['%s-instance_var' % country_code]
+        if not exists(secret_key_path):
+            _install_random_key(str(secret_key_path))
+
+        put(app['localrepo']/'fabfile'/'production-settings-%s.py' % country_code,
+            '%s/local_settings.py' % app['%s-flis_var' % country_code])
+
+    upload_template(app['localrepo']/'fabfile'/'countries-supervisord.conf',
+                    str(app['countries_sandbox']/'supervisord.conf'),
+                    context=app, backup=False)
+
+    if not exists(app['countries_sandbox']):
+        run("virtualenv --distribute '%(countries_sandbox)s'" % app)
+    run("%(countries_sandbox)s/bin/pip install -r %(countries_repo)s/pt/requirements.txt" % app)
+
+
+    for country_code in country_codes:
+        run("%s/bin/python %s/manage.py syncdb" % (
+                        app['countries_sandbox'], '%s/%s' % 
+                            (app['countries_repo'], country_code)
+                        )
+            )
+        run("%s/bin/python %s/manage.py migrate" % (
+                        app['countries_sandbox'], '%s/%s' %
+                            (app['countries_repo'], country_code)
+                        )
+            )
+
+@task
 def service(action):
     run("'%(sandbox)s/bin/supervisorctl' %(action)s %(name)s" % {
             'sandbox': app['sandbox'],
@@ -77,7 +125,29 @@ def service(action):
         })
 
 @task
+def countries_service(action):
+    for country_code in country_codes:
+        run("'%(sandbox)s/bin/supervisorctl' %(action)s %(name)s" % {
+                'sandbox': app['countries_sandbox'],
+                'action': action,
+                'name': 'flis_%s' % country_code,
+            })
+
+@task
 def deploy():
     execute('install')
     execute('service', 'restart')
+
+@task
+def deploy_countries():
+    execute('install_countries')
+    execute('countries_service', 'restart')
+
+@task
+def restart_countries():
+    execute('countries_service', 'restart')
+
+@task
+def stop_countries():
+    execute('countries_service', 'stop')
 
