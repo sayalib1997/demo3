0b88be2f9864a7dddd26ddf8bbc198fdb6ee0477
dumitval <dumitval@company.com>
2013-04-11 16:47:39 +0000

    #14248 FLIS instance GMT EU Ressource Efficiency

--- flis_django/fabfile/__init__.py
+++ flis_django/fabfile/__init__.py
@@ -17,6 +17,9 @@ app.update({
     'instance_var': app['repo']/'instance',
     'flis_var': app['repo']/'flis',
     'sandbox': app['repo']/'sandbox',
+    'article5_instance_var': app['article5_repo']/'instance',
+    'article5_flis_var': app['article5_repo']/'flis',
+    'article5_sandbox': app['article5_repo']/'sandbox',
     'countries_sandbox': app['countries_repo']/'sandbox',
     'user': 'edw',
 })
@@ -52,7 +55,7 @@ def _svn_repo(repo_path, origin_url, update=True):
             run("svn up")
 
 @task
-def install():
+def install_flis():
     _svn_repo(app['repo'], app['flis-repo'], update=True)
 
     if not exists(app['sandbox']):
@@ -79,6 +82,33 @@ def install():
     run("%s/bin/python %s/manage.py migrate" % (app['sandbox'], app['repo']))
 
 @task
+def install_article5():
+    _svn_repo(app['article5_repo'], app['flis-repo'], update=True)
+
+    if not exists(app['article5_sandbox']):
+        run("virtualenv --distribute '%(article5_sandbox)s'" % app)
+    run("%(article5_sandbox)s/bin/pip install -r %(article5_repo)s/requirements.txt" % app)
+
+    if not exists(app['article5_instance_var']):
+        run("mkdir -p '%(article5_instance_var)s'" % app)
+    if not exists(app['article5_instance_var']/'files'):
+        run("mkdir -p '%(article5_instance_var)s/files'" % app)
+
+    secret_key_path = app['article5_instance_var']/'secret_key.txt'
+    if not exists(secret_key_path):
+        _install_random_key(str(secret_key_path))
+
+    put(app['localrepo']/'fabfile'/'production-settings-article5.py',
+        str(app['article5_flis_var']/'local_settings.py'))
+
+    upload_template(app['localrepo']/'fabfile'/'supervisord-article5.conf',
+                    str(app['article5_sandbox']/'supervisord.conf'),
+                    context=app, backup=False)
+
+    run("%s/bin/python %s/manage.py syncdb" % (app['article5_sandbox'], app['article5_repo']))
+    run("%s/bin/python %s/manage.py migrate" % (app['article5_sandbox'], app['article5_repo']))
+
+@task
 def install_countries():
     for country_code in country_codes:
         _svn_repo(app['countries_repo']/country_code, app['flis-repo'], update=True)
@@ -95,7 +125,7 @@ def install_countries():
         put(app['localrepo']/'fabfile'/'production-settings-%s.py' % country_code,
             '%s/local_settings.py' % app['%s-flis_var' % country_code])
 
-    upload_template(app['localrepo']/'fabfile'/'countries-supervisord.conf',
+    upload_template(app['localrepo']/'fabfile'/'supervisord-countries.conf',
                     str(app['countries_sandbox']/'supervisord.conf'),
                     context=app, backup=False)
 
@@ -117,11 +147,19 @@ def install_countries():
             )
 
 @task
-def service(action):
+def service_flis(action):
     run("'%(sandbox)s/bin/supervisorctl' %(action)s %(name)s" % {
             'sandbox': app['sandbox'],
             'action': action,
             'name': 'flis_django',
+        })
+
+@task
+def service_article5(action):
+    run("'%(article5_sandbox)s/bin/supervisorctl' %(action)s %(name)s" % {
+            'sandbox': app['article5_sandbox'],
+            'action': action,
+            'name': 'flis_article5',
         })
 
 @task
@@ -134,9 +172,14 @@ def countries_service(action):
             })
 
 @task
-def deploy():
-    execute('install')
-    execute('service', 'restart')
+def deploy_flis():
+    execute('install_flis')
+    execute('service_flis', 'restart')
+
+@task
+def deploy_article5():
+    execute('install_article5')
+    execute('service_article5', 'restart')
 
 @task
 def deploy_countries():
