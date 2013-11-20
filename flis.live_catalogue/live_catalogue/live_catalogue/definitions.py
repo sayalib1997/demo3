bf7ff6d41c0bc082b99671d53a7d24120d97235d
dumitval <dumitval@company.com>
2013-11-20 16:15:13 +0000

    Merge branch 'master' of github.com:eaudeweb/flis.live_catalogue

---
+++ flis.live_catalogue/live_catalogue/live_catalogue/definitions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+CATEGORIES = (
+    ('article', 'Article'),
+    ('contracts', 'Contracts'),
+    ('open calls', 'Open Calls'),
+)
+
+
+FLIS_TOPICS = (
+    ('drivers and trends', 'Drivers and trends'),
+    ('indicators', 'Indicators'),
+)
+
+
+THEMES = (
+    ('water', 'Water'),
+    ('biodiversity', 'Biodiversity'),
+)
+
+
+GEOGRAPHIC_SCOPE = (
+    ('national', 'national (National; relates and covers one country)'),
+    ('subnational', 'subnational (Subnational; relates to a part of a country (in Europe often named as region))'),
+    ('regional', 'regional (Region in definition of UN (e.g. Pan-Europa))'),
+    ('transnational', 'transnational (Transnational; involves more than one country but not a full region)'),
+    ('global', 'global (Global; more than two global regions are involved)'),
+)
+
+
+COUNTRIES = (
+    ('ro', 'România'),
+)
