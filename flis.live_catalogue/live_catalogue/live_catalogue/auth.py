81fdcc3ee38bb9ff578be6004ff4e7daab2577f4
dumitval <dumitval@company.com>
2013-11-22 10:48:46 +0000

    Administrator also has access

--- flis.live_catalogue/live_catalogue/live_catalogue/auth.py
+++ flis.live_catalogue/live_catalogue/live_catalogue/auth.py
@@ -7,7 +7,8 @@ def login_required(f):
     @wraps(f)
     def wrapper(request, *args, **kwargs):
         if not getattr(settings, 'SKIP_EDIT_AUTHORIZATION', False):
-            if request.user_id and 'Contributor' in request.user_roles:
+            if request.user_id and ('Contributor' in request.user_roles
+                                    or 'Administrator' in request.user_roles):
                 pass
             else:
                return render(request, 'restricted.html')
