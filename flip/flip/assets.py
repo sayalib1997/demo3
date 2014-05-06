e1f76a16df611c1a78f92c0574c5cf99ae0e085a
catardra <catardra@company.com>
2014-05-06 12:19:32 +0000

    References and contact information section + calendars for start_date and end_date

--- flip/flip/assets.py
+++ flip/flip/assets.py
@@ -4,8 +4,17 @@ from django_assets import Bundle, register
 
 CSS_ASSETS = (
     'css/style.css',
+    'css/bootstrap-datetimepicker.css',
 )
 
 
+JS_ASSETS = (
+    'js/lib/moment.js',
+    'js/lib/bootstrap-datetimepicker.min.js',
+
+)
+
 css = Bundle(*CSS_ASSETS, filters='cssmin', output='packed.css')
+js = Bundle(*JS_ASSETS, filters='jsmin', output='packed.js')
 register('css', css)
+register('js', js)
