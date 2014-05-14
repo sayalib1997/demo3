11e2c43906836e21a2c994440719a4dbea4691c2
dumitval <dumitval@company.com>
2014-05-14 08:38:57 +0000

    removed common middleware from django settings

--- flip/flip/settings.py
+++ flip/flip/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE_CLASSES = (
     'frame.middleware.RequestMiddleware',
     'frame.middleware.UserMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
