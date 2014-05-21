565952bd0c86c4f2ffda49ca43d5a7780a51ac99
catardra <catardra@company.com>
2014-05-21 15:27:58 +0000

    Tests for study context and study metadata views

--- flip/flip/tests.py
+++ flip/flip/tests.py
@@ -1,13 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 
-from factory import DjangoModelFactory
+from factory import Factory, DjangoModelFactory
 from factory import fuzzy
 from factory import RelatedFactory, SubFactory
 
 from django.core.urlresolvers import reverse
+from django.db.models import Model
+from django.db.models.loading import get_model
+from django.test.utils import override_settings
 
 from django_webtest import WebTest
 from mock import Mock, patch
+from webtest.forms import Select, MultipleSelect
 
 from flip.models import Study
 
@@ -67,9 +71,160 @@ class StudyLanguageFactory(DjangoModelFactory):
     title = fuzzy.FuzzyText()
 
 
-class StudyMetadataPermissionTests(WebTest):
+class PhasesOfPolicyFactory(DjangoModelFactory):
+
+    FACTORY_FOR = 'flip.PhasesOfPolicy'
+
+    title = fuzzy.FuzzyText()
+
+
+class EnvironmentalThemeFactory(DjangoModelFactory):
+
+    FACTORY_FOR = 'flip.EnvironmentalTheme'
+
+    title = fuzzy.FuzzyText()
+
+
+class GeographicalScopeFactory(DjangoModelFactory):
+
+    FACTORY_FOR = 'flip.GeographicalScope'
+
+    title = fuzzy.FuzzyText()
+
+
+class StudyContextFactory(Factory):
+
+    ABSTRACT_FACTORY = True
+
+    purpose_and_target = fuzzy.FuzzyChoice([Study.POLICY, Study.NON_POLICY])
+
+    phases_of_policy = SubFactory(PhasesOfPolicyFactory)
+
+    geographical_scope = SubFactory(GeographicalScopeFactory)
+
+
+class BaseWebTest(WebTest):
 
     csrf_checks = False
+
+    def populate_fields(self, form, data):
+        for field_name, field in form.field_order:
+            if field_name in data:
+                value = data[field_name]
+                if isinstance(value, Model):
+                    value = value.pk
+                if isinstance(field, MultipleSelect):
+                    if not isinstance(value, list):
+                        value = [value]
+                if isinstance(field, (Select, MultipleSelect)):
+                    field.force_value(value)
+                else:
+                    field.value = value
+        return form
+
+    def normalize_data(self, data, date_input_format='%d/%m/%Y'):
+
+        def convert_model_to_pk(value):
+            if isinstance(value, Model):
+                return value.pk
+            return value
+
+        new_data = dict(data)
+        for k, v in new_data.items():
+            if isinstance(v, list):
+                new_data[k] = map(convert_model_to_pk, v)
+            elif isinstance(v, date):
+                new_data[k] = v.strftime(date_input_format)
+            else:
+                new_data[k] = convert_model_to_pk(v)
+        return new_data
+
+    def assertObjectInDatabase(self, model, **kwargs):
+        if isinstance(model, basestring):
+            app = kwargs.pop('app', None)
+            self.assertTrue(app)
+            Model = get_model(app, model)
+        else:
+            Model = model
+
+        if not Model:
+            self.fail('Model {} does not exist'.format(model))
+        try:
+            return Model.objects.get(**kwargs)
+        except Model.DoesNotExist:
+            self.fail('Object "{}" with kwargs {} does not exist'.format(
+                model, str(kwargs)
+            ))
+
+
+@override_settings(SKIP_EDIT_AUTHORIZATION=True, FRAME_URL=None)
+class StudyTests(BaseWebTest):
+
+    def test_study_new(self):
+        data = StudyFactory.attributes()
+        url = reverse('study_metadata_edit')
+        resp = self.app.get(url)
+        form = resp.forms['study-form']
+        self.populate_fields(form, self.normalize_data(data))
+        form.submit().follow()
+        self.assertObjectInDatabase(Study,
+                                    title=data['title'],
+                                    blossom=Study.NO,
+                                    lead_author=data['lead_author'])
+
+    def test_study_validate_blossom_and_requested_by(self):
+        data = StudyFactory.attributes(extra={'blossom': Study.YES})
+        url = reverse('study_metadata_edit')
+        resp = self.app.get(url)
+        form = resp.forms['study-form']
+        self.populate_fields(form, self.normalize_data(data))
+        resp = form.submit()
+        self.assertEqual(200, resp.status_int)
+        self.assertIn('start_date', resp.context['form'].errors)
+        self.assertIn('requested_by', resp.context['form'].errors)
+
+    def test_study_edit(self):
+        study = StudyFactory()
+        data = StudyFactory.attributes()
+        url = reverse('study_metadata_edit', kwargs={'pk': study.pk})
+        resp = self.app.get(url)
+        form = resp.forms['study-form']
+        data['title'] = 'new title'
+        self.populate_fields(form, self.normalize_data(data))
+        form.submit().follow()
+        self.assertObjectInDatabase(Study,
+                                    title=data['title'],
+                                    blossom=Study.NO,
+                                    lead_author=data['lead_author'])
+
+    def test_study_context_fail_if_not_blossom_edit(self):
+        study = StudyFactory()
+        url = reverse('study_context_edit', kwargs={'pk': study.pk})
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(404, resp.status_int)
+
+    def test_study_context_edit(self):
+        study = StudyFactory(blossom=Study.YES)
+        env = EnvironmentalThemeFactory()
+        data = StudyContextFactory.attributes(
+            extra={'environmental_themes': env},
+            create=True)
+        url = reverse('study_context_edit', kwargs={'pk': study.pk})
+        resp = self.app.get(url)
+        self.assertEqual(200, resp.status_int)
+        form = resp.forms['study-context-form']
+        self.populate_fields(form, self.normalize_data(data))
+        form.submit().follow()
+        self.assertObjectInDatabase(
+            Study,
+            purpose_and_target=data['purpose_and_target'],
+            phases_of_policy=data['phases_of_policy'],
+            environmental_themes=data['environmental_themes'],
+            geographical_scope=data['geographical_scope'])
+
+
+@override_settings(SKIP_EDIT_AUTHORIZATION=False, FRAME_URL=True)
+class StudyPermissionTests(BaseWebTest):
 
     @patch('frame.middleware.requests',
            Mock(return_value=UserAdminMock))
