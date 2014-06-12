1959f8e04d63e4b79150eceea47b38b7f91375be
catardra <catardra@company.com>
2014-06-12 14:42:07 +0000

    Navigating between tabs prompts user for unsaved changes

--- flip/flip/static/js/main.js
+++ flip/flip/static/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+    var alertOnExit = function () {
+        if(!window.onbeforeunload) {
+            window.onbeforeunload = function () {
+                return "You have unsaved changes. "
+            }
+        }
+    }
+
     $('[data-toggle=popover]').popover();
+
+    $('[data-edit-form]').on('keyup change', 'input, textarea', function () {
+        alertOnExit();
+    });
+    $('[data-edit-form]').on('click', 'button[type=submit]', function () {
+        window.onbeforeunload = null;
+    });
 
 });
