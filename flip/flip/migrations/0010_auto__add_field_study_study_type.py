83ab0ea24c2ce4c65f337a2f14e4d185dc323750
eftimie <eftimie@company.com>
2015-05-08 11:12:27 +0000

    [activities_evaluations] Migrate the Study model to include study_type, refs #23937

---
+++ flip/flip/migrations/0010_auto__add_field_study_study_type.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from south.db import db
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.add_column(u'flip_study', 'study_type',
+                      self.gf('django.db.models.fields.CharField')(max_length=128, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        db.delete_column(u'flip_study', 'study_type')
+
+
+    models = {
+        u'common.country': {
+            'Meta': {'object_name': 'Country'},
+            'is_deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'iso': ('django.db.models.fields.CharField', [], {'max_length': '2', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'common.environmentaltheme': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'EnvironmentalTheme'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'common.geographicalscope': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'GeographicalScope'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'require_country': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'flip.contenttopic': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'ContentTopic'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'flip.foresightapproaches': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'ForesightApproaches'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'flip.language': {
+            'Meta': {'object_name': 'Language'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '3', 'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        u'flip.outcome': {
+            'Meta': {'object_name': 'Outcome'},
+            'content_topic': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['flip.ContentTopic']", 'null': 'True', 'blank': 'True'}),
+            'document_title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'file_id': ('django.db.models.fields.files.FileField', [], {'default': "''", 'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'study': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'outcomes'", 'to': u"orm['flip.Study']"}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'type_of_outcome': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['flip.TypeOfOutcome']", 'null': 'True', 'blank': 'True'})
+        },
+        u'flip.phasesofpolicy': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'PhasesOfPolicy'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'flip.study': {
+            'Meta': {'object_name': 'Study'},
+            'additional_information': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'additional_information_foresight': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'additional_information_phase': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'additional_information_stakeholder': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'blossom': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'countries': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['common.Country']", 'symmetrical': 'False', 'blank': 'True'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'draft': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'environmental_themes': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['common.EnvironmentalTheme']", 'symmetrical': 'False', 'blank': 'True'}),
+            'foresight_approaches': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['flip.ForesightApproaches']", 'symmetrical': 'False'}),
+            'geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'languages': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['flip.Language']", 'through': u"orm['flip.StudyLanguage']", 'symmetrical': 'False'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'lead_author': ('django.db.models.fields.TextField', [], {}),
+            'other': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'phase_of_policy': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['flip.PhasesOfPolicy']", 'null': 'True', 'blank': 'True'}),
+            'purpose_and_target': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'requested_by': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'stakeholder_participation': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'study_type': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'user_id': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'})
+        },
+        u'flip.studylanguage': {
+            'Meta': {'object_name': 'StudyLanguage'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['flip.Language']"}),
+            'study': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['flip.Study']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'flip.typeofoutcome': {
+            'Meta': {'ordering': "('-pk',)", 'object_name': 'TypeOfOutcome'},
+            'blossom': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['flip']
